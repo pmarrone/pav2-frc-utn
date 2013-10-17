da13ce5d8ba1f53220d911db5c6f05f801f7bea5
florenciamarianamartina <florenciamarianamartina@gmail.com>
2013-10-17 23:48:24 +0000

    Arreglando momcos

---
+++ DAO/TipoMuebleDAO.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PatriaFabricaMuebles.Entidades;
+using System.Data.SqlClient;
+
+namespace PatriaFabricaMuebles.DAO
+{
+    public class TipoMuebleDAO
+    {
+        private static List<SqlParameter> LoadParameters(TipoMueble tipoMueble)
+        {
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            parameters.Add(new SqlParameter("id_tipo_mueble", tipoMueble.IdTipoMueble));
+            parameters.Add(new SqlParameter("descripcion", tipoMueble.Descrip));
+            return parameters;
+        }
+
+        private static TipoMueble ExtractData(SqlDataReader reader)
+        {
+            TipoMueble tipoMueble = new TipoMueble();
+            tipoMueble.IdTipoMueble = (int?)reader["id_tipo_mueble"];
+            tipoMueble.Descrip = (string)reader["descripcion"];
+
+            return tipoMueble;
+        }
+
+        public static TipoMueble GetById(int id)
+        {
+            TipoMueble material = new TipoMueble();
+            material.IdTipoMueble = id;
+            return GetById(material);
+        }
+        public static TipoMueble GetById(TipoMueble item)
+        {
+            string query = "SELECT [id_tipo_mueble]"
+                + " ,[descripcion]"
+                + " FROM [Muebles].[dbo].[Tipos_Mueble]"
+                + " WHERE id_tipo_mueble = @id_tipo_mueble ";
+            TipoMueble tipoMueble = null;
+            Dal.ExecuteReader(query, LoadParameters(item), delegate(SqlDataReader reader)
+            {
+                if (reader.Read())
+                {
+                    tipoMueble = ExtractData(reader);
+                }
+            });
+            return tipoMueble;
+        }
+
+        public static List<TipoMueble> GetAll()
+        {
+            string query = "SELECT [id_tipo_mueble]"
+                + " ,[descripcion]"
+                + " FROM [Muebles].[dbo].[Tipos_Mueble]";
+            List<TipoMueble> tiposMueble = new List<TipoMueble>();
+            Dal.ExecuteReader(query, null, delegate(SqlDataReader reader)
+            {
+                while (reader.Read())
+                {
+                    tiposMueble.Add(ExtractData(reader));
+                }
+            });
+            return tiposMueble;
+        }
+
+    }
+}
---
+++ DAO/UsuarioDAO.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PatriaFabricaMuebles.DAO
+{
+    public class UsuarioDAO
+    {
+        public static Boolean ValidarUsuario(string id_usuario, string password)
+        {
+            SqlDataReader reader = null;
+            SqlConnection connection = null;
+            SqlCommand cmd = new SqlCommand();
+
+            try
+            {
+                connection = new SqlConnection(ConnectionStringManager.ConnectionString);
+                String sql = "select * from Usuarios where id_usuario = @id_usuario and hashed_password = @hashed_password";
+
+                cmd.CommandText = sql;
+                cmd.Connection = connection;
+                cmd.CommandType = CommandType.Text;
+                //XXX: Falta hashear el password
+                cmd.Parameters.AddWithValue("@id_usuario", id_usuario);
+                cmd.Parameters.AddWithValue("@hashed_password", password);
+
+                connection.Open();
+                reader = cmd.ExecuteReader();
+
+                return reader.HasRows;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                cmd.Dispose();
+                connection.Close();
+                reader.Close();
+            }
+        }
+
+        public static string[] ObtenerRoles(string usuario)
+        {
+            switch (usuario.ToLower())
+            {
+                case "admin":
+                    return new string[] { "administradores" }; //permite ingresar a la pagina de registro de usuarios, es a modo ejemplo
+                case "empleado":
+                    return new string[] { "empleados" }; //solo ingresa a la pagina default
+                default:
+                    return new string[] { "clientes" }; //solo ingresa a la pagina default
+            }
+        }
+
+
+    }
+}
