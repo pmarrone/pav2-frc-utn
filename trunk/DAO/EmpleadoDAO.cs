45dcef33885694fe72cf0d50f6d3fa7ba8f63953
Cover.HM@gmail.com <Cover.HM@gmail.com@8f855346-a06a-9fbb-2212-9499ed2628b7>
2013-11-04 12:24:41 +0000

    EmpleadoDAO Updated!

--- trunk/DAO/EmpleadoDAO.cs
+++ trunk/DAO/EmpleadoDAO.cs
@@ -25,7 +25,7 @@ namespace PatriaFabricaMuebles.DAO
         /// <returns>void</returns>
         /// <Date>2011-07-15T15:15:05</Date>
         /// <Author>hcmoreno</Author>
-        public static int Insert(Empleado pEmpleado)
+        public static void Insert(Empleado pEmpleado)
         {
             SqlConnection connection = null;
 
@@ -34,8 +34,9 @@ namespace PatriaFabricaMuebles.DAO
             {
                 connection = new SqlConnection(CONNECTION_STRING);
 
-                String sql = "insert into empleados(legajo, dni,fecha_ingreso, nombres, apellidos) OUTPUT Inserted.legajo "
-                    + "values(@legajo, @dni,@fecha_ingreso, @nombres, @apellidos)";
+                String sql = "insert into empleados(legajo, dni,FechaAlta, nombre, apellido,FechaNac,FechaBaja) "
+                    + "values(@legajo, @dni,@FechaAlta, @nombre, @apellido,@FechaNac,@FechaBaja)";
+
 
 
                 cmd.CommandText = sql;
@@ -43,14 +44,17 @@ namespace PatriaFabricaMuebles.DAO
                 cmd.CommandType = CommandType.Text;
 
                 cmd.Parameters.AddWithValue("@legajo", pEmpleado.Legajo);
-                cmd.Parameters.AddWithValue("@dni", pEmpleado.NroDoc);
-                cmd.Parameters.AddWithValue("@fecha_ingreso", pEmpleado.FechaAlta);
-                cmd.Parameters.AddWithValue("@nombres", pEmpleado.Nombre);
-                cmd.Parameters.AddWithValue("@apellidos", pEmpleado.Apellido);
+                cmd.Parameters.AddWithValue("@dni", pEmpleado.Dni);
+                cmd.Parameters.AddWithValue("@FechaAlta", String.Format("{0:yyyyMMdd HH:mm:ss}", pEmpleado.FechaAlta));
+                cmd.Parameters.AddWithValue("@FechaNac", String.Format("{0:yyyyMMdd HH:mm:ss}", pEmpleado.FechaNac));
+                cmd.Parameters.AddWithValue("@FechaBaja", String.Format("{0:yyyyMMdd HH:mm:ss}", pEmpleado.FechaBaja));
+                cmd.Parameters.AddWithValue("@nombre", pEmpleado.Nombre);
+                cmd.Parameters.AddWithValue("@apellido", pEmpleado.Apellido);
 
                 connection.Open();
-                int idMagico = (int)cmd.ExecuteScalar();
-                return idMagico;
+                cmd.ExecuteScalar();
+
+
             }
             catch (Exception ex)
             {
@@ -80,18 +84,21 @@ namespace PatriaFabricaMuebles.DAO
                 strCnn = CONNECTION_STRING;
                 connection = new SqlConnection(strCnn);
 
-                String sql = "update empleados set dni =@dni, fecha_ingreso = @fecha_ingreso, nombres = @nombres, "
-                    + "apellidos = @apellidos where legajo=@legajo";
+                String sql = "update empleados set dni =@dni,FechaNac = @FechaNac,FechaBaja = @FechaBaja, FechaAlta = @FechaAlta, nombre = @nombre, "
+
+                    + "apellido = @apellido where legajo=@legajo";
 
                 cmd.CommandText = sql;
                 cmd.Connection = connection;
                 cmd.CommandType = CommandType.Text;
 
                 cmd.Parameters.AddWithValue("@legajo", pEmpleado.Legajo);
-                cmd.Parameters.AddWithValue("@dni", pEmpleado.NroDoc);
-                cmd.Parameters.AddWithValue("@fecha_ingreso", pEmpleado.FechaAlta);
-                cmd.Parameters.AddWithValue("@nombres", pEmpleado.Nombre);
-                cmd.Parameters.AddWithValue("@apellidos", pEmpleado.Apellido);
+                cmd.Parameters.AddWithValue("@dni", pEmpleado.Dni);
+                cmd.Parameters.AddWithValue("@FechaAlta", pEmpleado.FechaAlta);
+                cmd.Parameters.AddWithValue("@FechaNac", pEmpleado.FechaNac != null ? pEmpleado.FechaNac : (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("@FechaBaja", pEmpleado.FechaBaja != null ? pEmpleado.FechaBaja : (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("@nombre", pEmpleado.Nombre);
+                cmd.Parameters.AddWithValue("@apellido", pEmpleado.Apellido);
 
                 connection.Open();
                 cmd.ExecuteNonQuery();
@@ -161,7 +168,7 @@ namespace PatriaFabricaMuebles.DAO
             {
                 connection = new SqlConnection(CONNECTION_STRING);
 
-                String sql = "select legajo, dni, fecha_ingreso, nombres, apellidos from empleados";
+                String sql = "select nombre, apellido, dni,FechaNac,legajo, FechaAlta,FechaBaja from empleados";
 
 
                 cmd.CommandText = sql;
@@ -176,10 +183,13 @@ namespace PatriaFabricaMuebles.DAO
                 {
                     Empleado oEmpleado = new Empleado();
                     oEmpleado.Legajo = (Int32)reader["legajo"];
-                    oEmpleado.Nombre = (String)reader["nombres"];
-                    oEmpleado.Apellido = (String)reader["apellidos"];
-                    oEmpleado.FechaAlta = (DateTime)reader["fecha_ingreso"];
-                    oEmpleado.NroDoc = Convert.ToInt32(reader["dni"]);
+                    oEmpleado.Nombre = (String)reader["nombre"];
+                    oEmpleado.Apellido = (String)reader["apellido"];
+                    oEmpleado.FechaAlta = (DateTime)reader["FechaAlta"];
+                    oEmpleado.FechaBaja = reader["FechaBaja"] == DBNull.Value ? Convert.ToDateTime("01/01/2014") : (DateTime)reader["FechaBaja"];
+                    oEmpleado.FechaNac = reader["FechaNac"] == DBNull.Value ? Convert.ToDateTime("01/01/2014") : (DateTime)reader["FechaNac"];
+
+                    oEmpleado.Dni = Convert.ToInt32(reader["dni"]);
 
                     empleados.Add(oEmpleado);
                 }
@@ -204,7 +214,8 @@ namespace PatriaFabricaMuebles.DAO
             SqlDataReader reader = null;
             SqlConnection connection = null;
             SqlCommand cmd = new SqlCommand();
-            
+
+
             try
             {
                 connection = new SqlConnection(CONNECTION_STRING);
@@ -218,16 +229,17 @@ namespace PatriaFabricaMuebles.DAO
 
                 connection.Open();
                 reader = cmd.ExecuteReader();
-                
+
+
                 Empleado empleado = null;
-                if (reader.Read())               
+                if (reader.Read())
                 {
                     empleado = new Empleado();
                     empleado.Legajo = (Int32)reader["legajo"];
                     empleado.Nombre = (String)reader["nombres"];
                     empleado.Apellido = (String)reader["apellidos"];
                     empleado.FechaAlta = (DateTime)reader["fecha_ingreso"];
-                    empleado.NroDoc = Convert.ToInt32(reader["dni"]);
+                    empleado.Dni = Convert.ToInt32(reader["dni"]);
                 }
                 return empleado;
             }
@@ -242,5 +254,163 @@ namespace PatriaFabricaMuebles.DAO
                 reader.Close();
             }
         }
+
+        /// <summary>
+        /// GetAll
+        /// </summary>
+        /// <Author>hcmoreno</Author>
+        public static DataSet GetAllwithUserAndRol()
+        {
+            SqlConnection connection = null;
+            SqlCommand cmd = new SqlCommand();
+
+            try
+            {
+                connection = new SqlConnection(CONNECTION_STRING);
+
+                String sql = "select r.descripcion,u.id_rol,u.id_usuario as nombreUser,u.hashed_password,e.legajo,e.dni,e.FechaAlta,e.nombre,e.apellido,e.FechaNac as FechaNac,e.FechaBaja " +
+                                 "from Usuarios u,Empleados e,Roles r " +
+                                 "where e.legajo=u.legajo and u.id_rol=r.id_rol";
+
+                cmd.CommandText = sql;
+                cmd.Connection = connection;
+                cmd.CommandType = CommandType.Text;
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+
+                return ds;
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                cmd.Dispose();
+                connection.Close();
+            }
+        }
+
+        /// <summary>
+        /// GetGetRolEmpleado
+        /// </summary>
+        /// <Author>hcmoreno</Author>
+        public static DataSet GetRolEmpleado(string pUserName)
+        {
+            SqlConnection connection = null;
+            SqlCommand cmd = new SqlCommand();
+
+            try
+            {
+                connection = new SqlConnection(CONNECTION_STRING);
+
+                String sql = "select r.descripcion, " +
+                             "u.id_rol, " +
+                             "u.id_usuario as nombreUser, " +
+                             "u.hashed_password, " +
+                             "e.legajo, " +
+                             "e.dni, " +
+                             "e.FechaAlta, " +
+                             "e.nombre, " +
+                             "e.apellido, " +
+                             "e.FechaNac as FechaNac, " +
+                             "e.FechaBaja " +
+                             "from Usuarios u,Empleados e,Roles r " +
+                             "where e.legajo=u.legajo and " +
+                             "u.id_rol=r.id_rol and " +
+                             "(u.id_usuario=@id_usuario) ";
+                //"(e.dni=@dni or @dni is null)  and" +
+                //"(e.apellido=@apellido or @apellido is null) and " +
+                //"(e.nombre=@nombre or @nombre is null)  ";
+
+                cmd.CommandText = sql;
+                cmd.Connection = connection;
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@id_usuario", pUserName);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+
+                return ds;
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                cmd.Dispose();
+                connection.Close();
+            }
+        }
+
+        /// <summary>
+        /// GetGetRolEmpleado
+        /// </summary>
+        /// <Author>hcmoreno</Author>
+        public static DataSet GetByParam(Empleado pEmpleado)
+        {
+            SqlConnection connection = null;
+            SqlCommand cmd = new SqlCommand();
+
+            try
+            {
+                connection = new SqlConnection(CONNECTION_STRING);
+
+                String sql = "select r.descripcion, " +
+                             "u.id_rol, " +
+                             "u.id_usuario as nombreUser, " +
+                             "u.hashed_password, " +
+                             "e.legajo, " +
+                             "e.dni, " +
+                             "e.FechaAlta, " +
+                             "e.nombre, " +
+                             "e.apellido, " +
+                             "e.FechaNac as FechaNac, " +
+                             "e.FechaBaja " +
+                             "from Usuarios u,Empleados e,Roles r " +
+                             "where e.legajo=u.legajo and " +
+                             "u.id_rol=r.id_rol and " +
+                             "(e.legajo=@legajo or @legajo is null) and " +
+                             "(e.dni=@dni or @dni is null)  and" +
+                             "(e.apellido=@apellido or @apellido is null) and " +
+                             "(e.nombre=@nombre or @nombre is null)  ";
+
+                cmd.CommandText = sql;
+                cmd.Connection = connection;
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@legajo", pEmpleado.Legajo != null ? pEmpleado.Legajo : (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("@dni", pEmpleado.Dni != null ? pEmpleado.Dni : (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("@apellido", pEmpleado.Apellido != String.Empty ? pEmpleado.Apellido : (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("@nombre", pEmpleado.Nombre != String.Empty ? pEmpleado.Nombre : (object)DBNull.Value);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+
+                return ds;
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                cmd.Dispose();
+                connection.Close();
+            }
+        }
+
+
+
     }
 }
